8c01b82522b07f9d7bcba26b95b0e8fee31f1b00
codespool <4625220+codespool@users.noreply.github.com>
2025-10-30 13:37:36 +0200

    add subaccount call method

--- examples/testapp/src/components/RpcMethods/shortcut/walletTxShortcuts.ts
+++ examples/testapp/src/components/RpcMethods/shortcut/walletTxShortcuts.ts
@@ -1,6 +1,6 @@
 import { encodeFunctionData } from 'viem'
 import { ShortcutType } from './ShortcutType'
-import { ADDR_TO_FILL, CHAIN_ID_TO_FILL } from './const'
+import { ADDR_TO_FILL, CHAIN_ID_TO_FILL, SUBACCOUNT_ADDR_TO_FILL } from './const'
 
 const data = encodeFunctionData({
 	abi: [
@@ -22,6 +22,26 @@ const walletSendCallsShortcuts: ShortcutType[] = [
 		data: {
 			chainId: CHAIN_ID_TO_FILL,
 			from: ADDR_TO_FILL,
+			calls: [
+				{
+					to: '0x6bcf154A6B80fDE9bd1556d39C9bCbB19B539Bd8',
+					data,
+					value: '0x0',
+				},
+			],
+			version: '1',
+			capabilities: {
+				paymaster: {
+					url: 'https://paymaster.scs.startale.com/v1',
+				},
+			},
+		},
+	},
+	{
+		key: 'wallet_sendCalls (with subaccount)',
+		data: {
+			chainId: CHAIN_ID_TO_FILL,
+			from: SUBACCOUNT_ADDR_TO_FILL,
 			calls: [
 				{
 					to: '0x6bcf154A6B80fDE9bd1556d39C9bCbB19B539Bd8',
