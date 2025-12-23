fd12aca8945cba1c0c9c20e2975b87e56e9699c6
Bobo <bobo.kovacevic@gmail.com>
2025-12-23 21:13:31 +0100

    Add production endpoint to the config

--- examples/testapp/src/store/config.ts
+++ examples/testapp/src/store/config.ts
@@ -6,7 +6,8 @@ export type SDKVersionType = (typeof sdkVersions)[number]
 
 export const SELECTED_SCW_URL_KEY = 'scw_url'
 export const scwUrls = [
-	'https://deploy-sub-account-poc-for-yoake-sa-228.d3qb16qon2uoic.amplifyapp.com',
+	'https://app.startale.com/',
 	'http://localhost:3000/',
+	'https://keys.coinbase.com/connect'
 ] as const
 export type ScwUrlType = (typeof scwUrls)[number]
