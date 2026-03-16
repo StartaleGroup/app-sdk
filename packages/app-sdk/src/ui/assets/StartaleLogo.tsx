bd7ba89fde094af29c446f06ce98836c2810c99e
Bobo <bobo.kovacevic@gmail.com>
2026-03-16 09:00:45 +0100

    SVG update

--- packages/app-sdk/src/ui/assets/StartaleLogo.tsx
+++ packages/app-sdk/src/ui/assets/StartaleLogo.tsx
@@ -7,7 +7,7 @@ export const StartaleLogo = () => {
 				<path fill="#fff" d="M37.022 46.957 30 53.978l-7.02-7.02L30 39.935zM22.98 46.93h-9.93V37h9.93zm23.952 0h-9.93V37h9.93zM20.077 29.965l-7.02 7.021-7.022-7.021 7.022-7.02zm33.888 0-7.02 7.021-7.022-7.021 7.021-7.02zM22.98 22.93h-9.93V13h9.93zm14.022-9.928V13h9.93v9.93h-9.93v-9.89L30 20.042l-7.02-7.02L30 6z"/>
 			</g>
 			<defs>
-				<clipPath id="a"><path fill="#fff" d="M0 0h60v60H0z"/></clipPath>
+				<clipPath><path fill="#fff" d="M0 0h60v60H0z"/></clipPath>
 			</defs>
 		</svg>
 	)
